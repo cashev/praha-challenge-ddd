cb2a68cfd2761b1156b180dcc013791ef9d53a8c
cashev <42924021+cashev@users.noreply.github.com>
2023-02-13 14:17:48 +0000

    format

--- backend/src/domain/value-object/userEmail.ts
+++ backend/src/domain/value-object/userEmail.ts
@@ -14,13 +14,14 @@ export class UserEmail extends ValueObject<UserEmailProps> {
   }
 
   private static isValid(value: string): boolean {
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return emailRegex.test(value);
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value);
   }
 
   public static create(value: string): UserEmail {
     if (!this.isValid(value)) {
-      throw new Error("メールアドレスではありません。:" + value);
+      throw new Error('メールアドレスではありません。:' + value);
     }
     return new UserEmail({ value });
   }
