bd4d0b45b670f7e8485f6cbb24dff704e446c3c2
cashev <42924021+cashev@users.noreply.github.com>
2023-02-14 13:43:12 +0000

    fix regex

--- backend/src/domain/value-object/userEmail.ts
+++ backend/src/domain/value-object/userEmail.ts
@@ -15,7 +15,7 @@ export class UserEmail extends ValueObject<UserEmailProps> {
 
   private static isValid(value: string): boolean {
     const emailRegex =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(value);
   }
 
