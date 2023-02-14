cb9800ed47ce6e465e272d94ed05f67ba6f47d77
cashev <42924021+cashev@users.noreply.github.com>
2023-02-14 14:30:29 +0000

    format

--- backend/src/app/__tests__/get-all-user-usecase.test.ts
+++ backend/src/app/__tests__/get-all-user-usecase.test.ts
@@ -2,9 +2,21 @@ import { IUserQS, UserDto } from '../query-service-interface/user-qs';
 import { GetAllUserUseCase } from '../get-all-user-usecase';
 
 describe('do', () => {
-  const u1 = new UserDto({ id: 1, name: '永野 哲哉', email: 'tty-ngn19800814@dion.ne.jp' });
-  const u2 = new UserDto({ id: 2, name: '江口 隆吉', email: 'egt572@asp.home.ne.jp' });
-  const u3 = new UserDto({ id: 3, name: '柳沢 朋美', email: 'tmmyngsw@ybb.ne.jp' });
+  const u1 = new UserDto({
+    id: 1,
+    name: '永野 哲哉',
+    email: 'tty-ngn19800814@dion.ne.jp',
+  });
+  const u2 = new UserDto({
+    id: 2,
+    name: '江口 隆吉',
+    email: 'egt572@asp.home.ne.jp',
+  });
+  const u3 = new UserDto({
+    id: 3,
+    name: '柳沢 朋美',
+    email: 'tmmyngsw@ybb.ne.jp',
+  });
   const mockUserQS = new (class implements IUserQS {
     public getAll(): Promise<UserDto[]> {
       const users = [u1, u2, u3];
