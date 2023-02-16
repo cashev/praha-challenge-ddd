87375c385ce561a31a729183579599a14259523a
cashev <42924021+cashev@users.noreply.github.com>
2023-02-16 14:07:09 +0000

    fix UserStatus

--- backend/src/domain/value-object/userStatus.ts
+++ backend/src/domain/value-object/userStatus.ts
@@ -1,7 +1,7 @@
 const UserStatus = {
-  Zaiseki: '在籍',
-  Kyukai: '休会',
-  Taikai: '退会',
+  Zaiseki: '在籍中',
+  Kyukai: '休会中',
+  Taikai: '退会済',
 };
 
 export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus];
