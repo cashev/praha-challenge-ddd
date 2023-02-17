5d8c415af8f28cced9fd69837e4ae55e1b9839f1
cashev <42924021+cashev@users.noreply.github.com>
2023-02-17 13:52:00 +0000

    improve userstatus

--- backend/src/domain/value-object/userStatus.ts
+++ backend/src/domain/value-object/userStatus.ts
@@ -1,7 +1,22 @@
-const UserStatus = {
+const UserStatusValues = {
   Zaiseki: '在籍中',
   Kyukai: '休会中',
   Taikai: '退会済',
-};
+} as const;
 
-export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus];
+export const Zaiseki = Symbol();
+export const Kyukai = Symbol();
+export const Taikai = Symbol();
+export type UserStatus = typeof Zaiseki | typeof Kyukai | typeof Taikai;
+
+export function createUserStatus(status: string): UserStatus {
+  switch (status) {
+    case UserStatusValues.Zaiseki:
+      return Zaiseki;
+    case UserStatusValues.Kyukai:
+      return Kyukai;
+    case UserStatusValues.Taikai:
+      return Taikai;
+  }
+  throw Error('不正な値です。在籍中, 休会中, 退会済を指定してください。: ' + status);
+}
