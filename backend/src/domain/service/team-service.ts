8caf3b898117a95e338098391b5d708af3f666ff
cashev <42924021+cashev@users.noreply.github.com>
2023-02-19 15:28:21 +0000

    add TeamService isSameTeam Method

--- backend/src/domain/service/team-service.ts
+++ backend/src/domain/service/team-service.ts
@@ -1,4 +1,5 @@
 import { ITeamRepository } from 'src/app/repository-interface/team-repository';
+import { User } from '../entity/user';
 import { TeamName } from '../value-object/teamName';
 
 export class TeamService {
@@ -10,5 +11,13 @@ export class TeamService {
   async isDuplicated(teamName: TeamName): Promise<boolean> {
     const team = await this.teamRepo.findByName(teamName.value);
     return Promise.resolve(team !== null);
+  }
+
+  async isSameTeam(member: User[]): Promise<boolean> {
+    const team = await this.teamRepo.findByUserId(member[0].id);
+    const teamMemberIdSet = new Set(team.member.map((user) => user.id));
+    return Promise.resolve(
+      member.map((user) => user.id).every((id) => teamMemberIdSet.has(id)),
+    );
   }
 }
