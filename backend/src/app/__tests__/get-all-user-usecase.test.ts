7874d7e9b6d3d4bbb990e541d15b2de35763bb68
cashev <42924021+cashev@users.noreply.github.com>
2023-03-22 14:56:03 +0000

    fix lint

--- backend/src/app/__tests__/get-all-user-usecase.test.ts
+++ backend/src/app/__tests__/get-all-user-usecase.test.ts
@@ -25,9 +25,6 @@ describe('do', () => {
       const users = [u1, u2, u3];
       return Promise.resolve(users);
     }
-    findById(id: number): Promise<UserDto> {
-      return Promise.resolve(u1);
-    }
   })();
 
   test('[正常系]', async () => {
