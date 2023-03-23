9bab6c0b077a29e4dd13d3be6422ffa3aa4ee22a
cashev <42924021+cashev@users.noreply.github.com>
2023-03-23 14:31:27 +0000

    Team RepositoryにfindByParticipantIdメソッドを追加

--- backend/src/domain/repository-interface/team-repository.ts
+++ backend/src/domain/repository-interface/team-repository.ts
@@ -1,6 +1,7 @@
 import { Team } from 'src/domain/entity/team';
 
 export interface ITeamRepository {
+  findByParticipantId(participantId: number): Promise<Team>;
   getSmallestTeamList(): Promise<Team[]>;
   getNextPairId(): Promise<number>;
   save(team: Team): Promise<Team>;
