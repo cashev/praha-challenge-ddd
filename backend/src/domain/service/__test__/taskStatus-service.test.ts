ae8d2cfb5507540e92e766eba965402fe28e80d9
cashev <42924021+cashev@users.noreply.github.com>
2023-04-09 15:23:34 +0000

    format

--- backend/src/domain/service/__test__/taskStatus-service.test.ts
+++ backend/src/domain/service/__test__/taskStatus-service.test.ts
@@ -1,32 +1,35 @@
-import { ParticipantIdType } from "src/domain/entity/participant";
-import { TaskStatusService } from "../taskStatus-service";
-import { Yet } from "src/domain/value-object/taskStatusValue";
-import { number } from "fp-ts";
+import { ParticipantIdType } from 'src/domain/entity/participant';
+import { TaskStatusService } from '../taskStatus-service';
+import { Yet } from 'src/domain/value-object/taskStatusValue';
 
 describe('createAllTaskStatusForNewParticipant', () => {
-    const mockTaskStatusRepo = () => {
-        return {
-        find: jest.fn(),
-        getNextIdAndSetNext: jest.fn().mockResolvedValue(321),
-        save: jest.fn(),
-        }
-      };
+  const mockTaskStatusRepo = () => {
+    return {
+      find: jest.fn(),
+      getNextIdAndSetNext: jest.fn().mockResolvedValue(321),
+      save: jest.fn(),
+      saveAll: jest.fn(),
+    };
+  };
 
-    test('[正常系]', async () => {
-        const taskIds = [];
-        for (let i = 1; i <= 80; i++) {
-            taskIds.push(i);
-        };
-        const service = new TaskStatusService(mockTaskStatusRepo());
-        const result = await service.createTaskStatusForNewParticipant(4 as ParticipantIdType, taskIds);
-        expect(result.length).toBe(80);
-        expect(result.every(ts => ts.status === Yet)).toBeTruthy();
-        expect(Math.min(...result.map(ts => ts.id as number))).toBe(321);
-        expect(Math.max(...result.map(ts => ts.id as number))).toBe(400);
-        expect(new Set(result.map(ts => ts.id)).size).toBe(80);
-        expect(result.every(ts => ts.participantId === 4)).toBeTruthy();
-        expect(Math.min(...result.map(ts => ts.taskId as number))).toBe(1);
-        expect(Math.max(...result.map(ts => ts.taskId as number))).toBe(80);
-        expect(new Set(result.map(ts => ts.taskId)).size).toBe(80);
-    });
+  test('[正常系]', async () => {
+    const taskIds = [];
+    for (let i = 1; i <= 80; i++) {
+      taskIds.push(i);
+    }
+    const service = new TaskStatusService(mockTaskStatusRepo());
+    const result = await service.createTaskStatusForNewParticipant(
+      4 as ParticipantIdType,
+      taskIds,
+    );
+    expect(result.length).toBe(80);
+    expect(result.every((ts) => ts.status === Yet)).toBeTruthy();
+    expect(Math.min(...result.map((ts) => ts.id as number))).toBe(321);
+    expect(Math.max(...result.map((ts) => ts.id as number))).toBe(400);
+    expect(new Set(result.map((ts) => ts.id)).size).toBe(80);
+    expect(result.every((ts) => ts.participantId === 4)).toBeTruthy();
+    expect(Math.min(...result.map((ts) => ts.taskId as number))).toBe(1);
+    expect(Math.max(...result.map((ts) => ts.taskId as number))).toBe(80);
+    expect(new Set(result.map((ts) => ts.taskId)).size).toBe(80);
+  });
 });
