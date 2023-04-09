4cacf2a62fc58b6a36d06c51e4c0e0cd6e6de8bc
cashev <42924021+cashev@users.noreply.github.com>
2023-04-09 09:29:51 +0000

    format

--- backend/src/app/query-service-interface/pair-qs.ts
+++ backend/src/app/query-service-interface/pair-qs.ts
@@ -1,15 +1,14 @@
 export class PairDto {
-    public readonly id: number;
-    public readonly name: string;
-  
-    public constructor(props: { id: number; name: string }) {
-      const { id, name } = props;
-      this.id = id;
-      this.name = name;
-    }
+  public readonly id: number;
+  public readonly name: string;
+
+  public constructor(props: { id: number; name: string }) {
+    const { id, name } = props;
+    this.id = id;
+    this.name = name;
   }
-  
-  export interface IPairQS {
-    getAll(): Promise<PairDto[]>;
-  }
-  
+}
+
+export interface IPairQS {
+  getAll(): Promise<PairDto[]>;
+}
