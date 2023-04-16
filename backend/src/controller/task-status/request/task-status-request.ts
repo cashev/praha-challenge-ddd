98f1335054c691b63a035029fd222c79041728db
cashev <42924021+cashev@users.noreply.github.com>
2023-04-16 11:40:06 +0000

    PatchからPostへ変更

---
+++ backend/src/controller/task-status/request/task-status-request.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class TaskStatusPostRequest {
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly taskIds: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsIn(['在籍中', 'レビュー待ち', '完了'])
+  readonly status: string;
+
+  @ApiProperty()
+  @IsNumber()
+  readonly page: number;
+}
--- backend/src/controller/task-status/task-status.controller.ts
+++ backend/src/controller/task-status/task-status.controller.ts
@@ -2,26 +2,24 @@ import { PrismaClient } from '@prisma/client';
 import { GetByTaskStatusResponse } from './response/task-status-response';
 import { ParticipantByTaskStatusQS } from 'src/infra/db/query-service/participant-by-taskStatus-qs';
 import { GetByTaskStatusUsecase } from 'src/app/get-by-taskStatus-usecase';
-import { Controller, Get, ParseArrayPipe, Query } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
+import { TaskStatusPostRequest } from './request/task-status-request';
 
 @Controller({
   path: '/taskStatus',
 })
 export class TaskStatusController {
-  @Get()
+  @Post()
   @ApiResponse({ status: 200, type: GetByTaskStatusResponse })
   async getByTaskStatus(
-    @Query('taskIds', new ParseArrayPipe({ items: String, separator: ',' }))
-    taskIds: string[],
-    @Query('status')
-    status: string,
-    @Query('page')
-    page?: number,
+    @Body()
+    taskStatusPostRequest: TaskStatusPostRequest,
   ): Promise<GetByTaskStatusResponse> {
     const prisma = new PrismaClient();
     const qs = new ParticipantByTaskStatusQS(prisma);
     const usecase = new GetByTaskStatusUsecase(qs);
+    const { taskIds, status, page } = taskStatusPostRequest;
     const result = await usecase.do(taskIds, status, page);
     const response = new GetByTaskStatusResponse({ participants: result });
     return response;
