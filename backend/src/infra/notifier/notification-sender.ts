ec61c6884057e93f2e9185a04d630a003f9b3153
cashev <42924021+cashev@users.noreply.github.com>
2023-04-18 14:29:29 +0000

    format

--- backend/src/infra/notifier/notification-sender.ts
+++ backend/src/infra/notifier/notification-sender.ts
@@ -1,25 +1,25 @@
-import { Notification } from "src/domain/entity/notification";
-import { INotificationSender } from "src/domain/notifier-interface/notification-sender";
-import * as nodemailer from "nodemailer";
+import { Notification } from 'src/domain/entity/notification';
+import { INotificationSender } from 'src/domain/notifier-interface/notification-sender';
+import * as nodemailer from 'nodemailer';
 
 export class NotificationSender implements INotificationSender {
-    private smtp: nodemailer.Transporter;
+  private smtp: nodemailer.Transporter;
 
-    public constructor() {
-        this.smtp = nodemailer.createTransport({
-            host: 'localhost',
-            port: 1025,
-        });
-    }
+  public constructor() {
+    this.smtp = nodemailer.createTransport({
+      host: 'localhost',
+      port: 1025,
+    });
+  }
 
-    async sendToAdmin(notification: Notification): Promise<void> {
-        const message = {
-            from: 'from@example.com',
-            to: 'to@example.com',
-            subject: notification.title,
-            text: notification.content,
-        };
+  async sendToAdmin(notification: Notification): Promise<void> {
+    const message = {
+      from: 'from@example.com',
+      to: 'to@example.com',
+      subject: notification.title,
+      text: notification.content,
+    };
 
-        this.smtp.sendMail(message);
-    }
+    this.smtp.sendMail(message);
+  }
 }
