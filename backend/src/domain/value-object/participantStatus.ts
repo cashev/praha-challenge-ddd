346e02a42f55f8b0491eb55a6fcb13e9435fca1b
cashev <42924021+cashev@users.noreply.github.com>
2023-04-24 14:38:28 +0000

    add comment

--- backend/src/domain/value-object/participantStatus.ts
+++ backend/src/domain/value-object/participantStatus.ts
@@ -4,23 +4,21 @@ const ParticipantStatusValues = {
   Taikai: '退会済',
 } as const;
 
+/** 在籍中 */
 export const Zaiseki = Symbol(ParticipantStatusValues.Zaiseki);
+/** 休会中 */
 export const Kyukai = Symbol(ParticipantStatusValues.Kyukai);
+/** 退会済 */
 export const Taikai = Symbol(ParticipantStatusValues.Taikai);
+/** 参加者在籍ステータス */
 export type ParticipantStatus = typeof Zaiseki | typeof Kyukai | typeof Taikai;
 
-export function getParticipantStatusValue(status: ParticipantStatus): string {
-  switch (status) {
-    case Zaiseki:
-      return ParticipantStatusValues.Zaiseki;
-    case Kyukai:
-      return ParticipantStatusValues.Kyukai;
-    case Taikai:
-      return ParticipantStatusValues.Taikai;
-  }
-  throw new Error();
-}
-
+/**
+ * 文字列から参加者在籍ステータスの型へ変換します。
+ *
+ * @param status 参加者在籍ステータスの文字列
+ * @returns 参加者在籍ステータスの型
+ */
 export function createUserStatus(status: string): ParticipantStatus {
   switch (status) {
     case ParticipantStatusValues.Zaiseki:
@@ -33,4 +31,21 @@ export function createUserStatus(status: string): ParticipantStatus {
   throw Error(
     '不正な値です。在籍中, 休会中, 退会済を指定してください。: ' + status,
   );
+}
+
+/**
+ * 参加者在籍ステータスの型から文字列へ変換します。
+ *
+ * @param status 参加者在籍ステータスの型
+ * @returns 参加者在籍ステータスの文字列
+ */
+export function getParticipantStatusValue(status: ParticipantStatus): string {
+  switch (status) {
+    case Zaiseki:
+      return ParticipantStatusValues.Zaiseki;
+    case Kyukai:
+      return ParticipantStatusValues.Kyukai;
+    case Taikai:
+      return ParticipantStatusValues.Taikai;
+  }
 }
--- backend/src/domain/value-object/taskStatusValue.ts
+++ backend/src/domain/value-object/taskStatusValue.ts
@@ -4,11 +4,21 @@ export const TaskStatusValues = {
   Done: '完了',
 } as const;
 
+/** 未着手 */
 export const Yet = Symbol();
+/** レビュー待ち */
 export const Waiting = Symbol();
+/** 完了 */
 export const Done = Symbol();
+/** 課題進捗ステータス */
 export type TaskStatusValue = typeof Yet | typeof Waiting | typeof Done;
 
+/**
+ * 文字列から課題進捗ステータスの型へ変換します。
+ *
+ * @param status 課題進捗ステータスの文字列
+ * @returns 課題進捗ステータスの型
+ */
 export function createTaskStatusValue(status: string): TaskStatusValue {
   switch (status) {
     case TaskStatusValues.Yet:
@@ -23,6 +33,12 @@ export function createTaskStatusValue(status: string): TaskStatusValue {
   );
 }
 
+/**
+ * 課題進捗ステータスの型から文字列へ変換します。
+ *
+ * @param taskStatus 課題進捗ステータスの型
+ * @returns 課題進捗ステータスの文字列
+ */
 export const getTaskStatusValue = (taskStatus: TaskStatusValue) => {
   switch (taskStatus) {
     case Yet:
