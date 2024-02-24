2d89bc09ffcebfcaa05d540b009725d07a026273
cashev <42924021+cashev@users.noreply.github.com>
2024-02-24 17:31:53 +0900

    fix eslint

--- backend/src/query/infra/db/__tests__/task-qs.integration.test.ts
+++ backend/src/query/infra/db/__tests__/task-qs.integration.test.ts
@@ -1,35 +1,35 @@
-import { TaskQS } from "../task-qs";
+import { TaskQS } from '../task-qs';
 import { prisma } from 'src/testUtil/prisma';
 import { createTestTask } from 'src/testUtil/test-data';
 
 describe('task-qs.integration.test', () => {
-    const taskQS = new TaskQS(prisma);
+  const taskQS = new TaskQS(prisma);
 
-    const deleteTestData = async () => {
-        await prisma.task.deleteMany();
-    };
+  const deleteTestData = async () => {
+    await prisma.task.deleteMany();
+  };
 
+  beforeAll(async () => {
+    await deleteTestData();
+  });
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  describe('getAll', () => {
     beforeAll(async () => {
-        await deleteTestData();
-    });
-    afterAll(async () => {
-        await prisma.$disconnect();
+      await prisma.task.createMany({ data: createTestTask() });
     });
 
-    describe('getAll', () => {
-        beforeAll(async () => {
-            await prisma.task.createMany({ data: createTestTask() });
-        });
-
-        test('', async () => {
-            const result = await taskQS.getAll();
-            expect(result.length).toEqual(4);
-            expect(result).toEqual([
-                { id: 'T001', title: '課題001', content: '内容001' },
-                { id: 'T002', title: '課題002', content: '内容002' },
-                { id: 'T003', title: '課題003', content: '内容003' },
-                { id: 'T004', title: '課題004', content: '内容004' },
-            ]);
-        });
+    test('', async () => {
+      const result = await taskQS.getAll();
+      expect(result.length).toEqual(4);
+      expect(result).toEqual([
+        { id: 'T001', title: '課題001', content: '内容001' },
+        { id: 'T002', title: '課題002', content: '内容002' },
+        { id: 'T003', title: '課題003', content: '内容003' },
+        { id: 'T004', title: '課題004', content: '内容004' },
+      ]);
     });
+  });
 });
