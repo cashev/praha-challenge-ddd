d2eee130edcde222a891d639dfde782cd638e2ea
cashev <42924021+cashev@users.noreply.github.com>
2024-07-06 12:21:15 +0900

    format

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,35 +11,37 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="mt-8 text-center">Firebase Auth with Next.js</h1>
       <div className="mb-8 text-center">
-        {user ? 
-        (
-          <>
-            <div>ログイン中: {user.email}</div>
-            <SignOut />
-          </>
-        ) : (
-          <>
-            <nav>
-              <ul>
-                <li>
-                  <Link href="/signin">ログイン</Link>
-                </li>
-                <li>
-                  <Link href="/signup">新規登録</Link>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <Link href="/team">チーム一覧</Link>
-                </li>
-                <li>
-                  <Link href="/pair">ペア一覧</Link>
-                </li>
-              </ul>
-            </nav>
-          </>
-        )}
+        {user ?
+          (
+            <>
+              <div>ログイン中: {user.email}</div>
+              <SignOut />
+            </>
+          ) : (
+            <>
+              <nav>
+                <ul>
+                  <li>
+                    <Link href="/signin">ログイン</Link>
+                  </li>
+                  <li>
+                    <Link href="/signup">新規登録</Link>
+                  </li>
+                </ul>
+              </nav>
+            </>
+          )}
       </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/team">チーム一覧</Link>
+          </li>
+          <li>
+            <Link href="/pair">ペア一覧</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
